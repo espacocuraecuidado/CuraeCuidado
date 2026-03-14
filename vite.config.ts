2ac5657997076d465958550c336c93e35b4df318
espacocuraecuidado <espacocuraecuidado@gmail.com>
2026-03-14 19:25:06 -0300

    import { defineConfig } from "vite";

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: "all", // Esta linha libera o acesso pelo túnel
     hmr: {
       overlay: false,
     },
